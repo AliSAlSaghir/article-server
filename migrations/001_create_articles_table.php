3342b3592c1b203696a54c245f54eab1944d95ac
Ali AlSaghir <alisalsaghir@gmail.com>
2025-07-04 13:49:30 +0300

    remove pivot table article_categories and make necessary fixes

--- migrations/001_create_articles_table.php
+++ migrations/001_create_articles_table.php
@@ -4,10 +4,12 @@ require("../connection/connection.php");
 $query = "CREATE TABLE articles(
           id INT AUTO_INCREMENT PRIMARY KEY,
           title VARCHAR(100) NOT NULL,
-          author INT NOT NULL,
+          author_id INT NOT NULL,
+          category_id INT NOT NULL,
           description TEXT NOT NULL,
           
-          FOREIGN KEY (author) REFERENCES users(id) ON DELETE CASCADE
+          FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE,
+          FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
           )";
 
 if ($mysqli->query($query)) {
